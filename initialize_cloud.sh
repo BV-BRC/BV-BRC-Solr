6f0afd740ba532fb1de00c94989f4b4b225d3a8d
Hyunseung Yoo <hsyoo@anl.gov>
2019-02-25 20:18:42 -0600

    fix typo

--- initialize_cloud.sh
+++ initialize_cloud.sh
@@ -17,7 +17,7 @@
 # curl 'localhost:8983/solr/admin/collections?action=ADDREPLICA&collection=genome_feature&shard=shard1&node=130.202.148.146:6765_solr'
 # curl 'localhost:8983/solr/admin/collections?action=ADDREPLICA&collection=genome_feature&shard=shard2&node=130.202.148.146:5654_solr'
 
-collections=(antibiotics enzyme_class_ref feature_sequence gene_ontology_ref genome genome_amr genome_feature genome_sequence id_ref misc_niaid_gsc model_complex_role model_compound model_reaction model_template_biomass model_template_reaction pathway pathway_ref ppi protein_family_ref sp_gene sp_gene_evidence sp_gene_ref structured_assertion subsystem subsystem_ref taxonomy transcriptomics_experiment transcriptomics_gene transcriptomics_sample)
+collections=(antibiotics enzyme_class_ref feature_sequence gene_ontology_ref genome genome_amr genome_feature genome_sequence id_ref misc_niaid_sgc model_complex_role model_compound model_reaction model_template_biomass model_template_reaction pathway pathway_ref ppi protein_family_ref sp_gene sp_gene_evidence sp_gene_ref structured_assertion subsystem subsystem_ref taxonomy transcriptomics_experiment transcriptomics_gene transcriptomics_sample)
 # collections=(genome genome_feature genome_sequence feature_sequence pathway subsystem)
 # collections=(genome)
 for collection in "${collections[@]}"
