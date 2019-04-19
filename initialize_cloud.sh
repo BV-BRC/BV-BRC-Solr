3baefa41ec92ee72cc2c44c9836df661ecea617f
Hyunseung Yoo <hsyoo@anl.gov>
2019-04-19 10:58:07 -0500

    remove unsed. reflect changes from prod solr

--- initialize_cloud.sh
+++ initialize_cloud.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# untar solr-7.5.0.tgz
-# mkdir node1 node2
-# cp solr-7.5.0/server/solr/solr.xml solr-7.5.0/server/solr/zoo.cfg node1
-# cp solr-7.5.0/server/solr/solr.xml solr-7.5.0/server/solr/zoo.cfg node2
-# overwrite SOLR_ULIMIT_CHECKS to 65000 in solr.in.sh
-# ./solr7/bin/solr start -c -p 8983 -s _PATH_TO_node1
-# ./solr7/bin/solr start -c -p 7574 -z localhost:9983 -s _PATH_TO_node2
-
-# add more nodes for replica
-# mkdir node3 node4
-# cp solr-7.5.0/server/solr/solr.xml solr-7.5.0/server/solr/zoo.cfg node3
-# cp solr-7.5.0/server/solr/solr.xml solr-7.5.0/server/solr/zoo.cfg node4
-# ./solr7/bin/solr start -c -p 6765 -z localhost:9983 -s /Users/hsyoo/Applications/solr_cloud/node3
-# ./solr7/bin/solr start -c -p 5654 -z localhost:9983 -s /Users/hsyoo/Applications/solr_cloud/node4
-# curl 'localhost:8983/solr/admin/collections?action=ADDREPLICA&collection=genome_feature&shard=shard1&node=130.202.148.146:6765_solr'
-# curl 'localhost:8983/solr/admin/collections?action=ADDREPLICA&collection=genome_feature&shard=shard2&node=130.202.148.146:5654_solr'
-
 # 16 static collections
 collections=(antibiotics enzyme_class_ref gene_ontology_ref misc_niaid_sgc model_complex_role model_compound model_reaction model_template_biomass model_template_reaction pathway_ref sp_gene_evidence sp_gene_ref subsystem_ref taxonomy transcriptomics_experiment transcriptomics_sample)
 for collection in "${collections[@]}"
