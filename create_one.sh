1377281b70a0004281c68750b43264284453a9e5
Harry Yoo <hsyoo@anl.gov>
2021-08-27 11:35:05 -0500

    update fieldtype and other scripts

--- create_one.sh
+++ create_one.sh
@@ -2,6 +2,7 @@
 
 collection=${1}
 mode=${2}
+shards=${3}
 
 # register configset
 (cd $collection && zip -r - *) | \
@@ -13,5 +14,5 @@ if [ $mode == "static" ]
 then
     curl "http://localhost:8983/solr/admin/collections?action=CREATE&name=${collection}&numShards=1&tlogReplicas=3&maxShardsPerNode=1&collection.configName=${collection}_set"
 else
-    curl "http://localhost:8983/solr/admin/collections?action=CREATE&name=${collection}&numShards=3&tlogReplicas=2&maxShardsPerNode=2&collection.configName=${collection}_set&policy=policy1"
+    curl "http://localhost:8983/solr/admin/collections?action=CREATE&name=${collection}&numShards=${shards}&tlogReplicas=1&nrtReplicas=0&maxShardsPerNode=1&collection.configName=${collection}_set"
 fi
